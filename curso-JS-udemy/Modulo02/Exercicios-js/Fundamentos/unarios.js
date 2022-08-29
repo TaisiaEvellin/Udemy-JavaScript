1040239bdf612ad624a9d054ebff0a2d5ac7edc8
TaisiaEvellin <taievellin26@gmail.com>
2022-08-29 16:24:47 -0300

    Commit all course

---
+++ curso-JS-udemy/Modulo02/Exercicios-js/Fundamentos/unarios.js
@@ -0,0 +1,15 @@
+let num1 = 1;
+let num2 = 2;
+
+num1++ // acrescenta uma unidade
+console.log(num1);
+--num1 // subtrai uma unidade
+console.log(num1);
+
+console.log(++num1 === num2--);
+// console.log(++num1 === --num2);
+console.log(num1 == num2);
+
+
+
+
